a52593980cad3d8c954e3fe0110c679c064bbbd8
kcshellacool <hudi1007@gmail.com>
2021-11-03 22:41:07 -0700

    npm installed and added ids and serviceworker.js

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "Budget_Tracker_Cache_v1";
+const DATA_CACHE = "data_cache_name_v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/js/idb.js",
+  "/js/index.js",
+  "/manifest.json",
+  "/css/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+];
+
+// Cache resources
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Respond with cached resources
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((error) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((error) => console.log(error))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
